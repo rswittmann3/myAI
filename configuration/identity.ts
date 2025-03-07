f90d19335625b9a0375a40dcb581a9c556c5d40b
Robert Wittmann <rswittmann3@gmail.com>
2025-03-07 15:22:29 -0500

    Update identity.ts
    identity

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Robert`;
 export const OWNER_DESCRIPTION: string = `Student`;
 
 export const AI_NAME: string = `Finnian`;
-export const AI_TONE: string = `Helpful and Clear`;
-export const AI_ROLE: string = `Financial Advisor`;
+export const AI_TONE: string = `Helpful, Clear, Thorough`;
+export const AI_ROLE: string = `Personal Financial Advisor`;
